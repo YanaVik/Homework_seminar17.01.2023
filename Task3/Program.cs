51ae7c0d7a1fcd63236a30da8c7e4c9ac67b2ae2
yvikh <y.vikhrova@inbox.ru>
2023-01-18 17:27:40 +0300

    Add Task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите порядковый номер дня недели от 1 до 7:");
+string a = Console.ReadLine();
+
+switch(a)
+{
+    case "1":
+        Console.WriteLine("Понедельник - рабочий день");
+    break;
+
+    case "2":
+        Console.WriteLine("Вторник - рабочий день");
+    break;
+
+    case "3":
+        Console.WriteLine("Среда - рабочий день");
+    break;
+
+    case "4":
+        Console.WriteLine("Четверг - рабочий день");
+    break;
+
+    case "5":
+        Console.WriteLine("Пятница - рабочий день");
+    break;
+
+    case "6":
+        Console.WriteLine("Суббота - выходной день. УРА!");
+    break;
+
+    case "7":
+        Console.WriteLine("Воскресенье - выходной день. УРА!");
+    break;
+
+}
